a0b17a9dd830eec7a5fa0f3a68934e80d1ee8895
supri <supri@Suprita-Chavan>
2026-04-24 14:43:55 +0530

    print class

---
+++ src/test/java/sample/Print.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi friend");
+		System.out.println("Bye friend");
+	}
+
+}
